8bdb4a881b86e5ce5729c28a250abec7378a73c8
klangner <klangner@gmail.com>
2016-06-14 20:30:18 +0200

    Added metadata update

---
+++ src/main/scala/pl/klangner/imeditor/ImagePanel.scala
@@ -0,0 +1,43 @@
+package pl.klangner.imeditor
+
+import java.awt.{Color, Dimension}
+import java.awt.image.BufferedImage
+import java.io.File
+import javax.imageio.ImageIO
+import scala.swing.{Graphics2D, Panel}
+
+class ImagePanel extends Panel
+{
+  private var _imagePath = ""
+  private var box = (0, 0, 0, 0)
+  private var bufferedImage:BufferedImage = null
+
+  def imagePath = _imagePath
+
+  def imagePath_=(value:String) {
+    _imagePath = value
+    bufferedImage = ImageIO.read(new File(_imagePath))
+    preferredSize = new Dimension(bufferedImage.getWidth, bufferedImage.getHeight)
+    repaint()
+  }
+
+  def setBoxPosition(left: Int, top: Int, right: Int, bottom: Int): Unit = {
+    box = (left, top, right, bottom)
+    repaint()
+  }
+
+  override def paintComponent(g:Graphics2D) = {
+    super.paintComponent(g)
+    if (null != bufferedImage) {
+      val w = 2 * bufferedImage.getWidth
+      val h = 2 * bufferedImage.getHeight
+      g.drawImage(bufferedImage, 0, 0, w, h, null)
+      g.setColor(Color.red)
+      g.drawRect(box._1, box._2, box._3-box._1, box._4-box._2)
+    }
+  }
+}
+
+object ImagePanel{
+  def apply() = new ImagePanel()
+}
--- src/main/scala/pl/klangner/imeditor/MainApp.scala
+++ src/main/scala/pl/klangner/imeditor/MainApp.scala
@@ -3,10 +3,13 @@ package pl.klangner.imeditor
 import java.io.File
 import javax.swing.JFileChooser
 
-import scala.collection.mutable.ListBuffer
+import scala.collection.mutable
 import scala.swing.BorderPanel.Position
 import scala.swing._
-import scala.swing.event.{ButtonClicked, SelectionChanged, EditDone}
+import scala.swing.event.{ButtonClicked, EditDone, MouseClicked}
+
+
+case class ImageMetadata(left: Int, top: Int, right: Int, bottom: Int, plateText: String)
 
 /**
   * @author Krzysztof Langner on 2016-06-14.
@@ -14,16 +17,18 @@ import scala.swing.event.{ButtonClicked, SelectionChanged, EditDone}
 object MainApp extends SimpleSwingApplication {
 
   val loadFolderButton = new Button("Load")
-  val imageList = new ListView(ListBuffer[String]("           "))
-  val leftField = new TextField(5)
-  val topField = new TextField(5)
-  val rightField = new TextField(5)
-  val bottomField = new TextField(5)
-  val textField = new TextField(10)
-  val imageCanvas = new Button("Image")
-  val prevButton = new Button("<---")
+  val leftTopField = new TextField(10)
+  val rightBottomField = new TextField(10)
+  val plateTextField = new TextField(10)
+  val imagePanel = new ImagePanel()
+  val prevButton = new Button("<--")
   val posLabel = new Label("0/0")
-  val nextButton = new Button("--->")
+  val nextButton = new Button("-->")
+
+  // State
+  val imageList = mutable.ArrayBuffer[File]()
+  val metadataList = mutable.HashMap[String, ImageMetadata]()
+  var currentImageIndex = 0
 
   def top = new MainFrame {
     title = "Image Metadata Editor"
@@ -32,23 +37,32 @@ object MainApp extends SimpleSwingApplication {
     contents = new BorderPanel {
 
       add(new FlowPanel(FlowPanel.Alignment.Left)(loadFolderButton), Position.North)
-      add(new SplitPane(Orientation.Vertical, new ScrollPane(imageList), editorForm), Position.Center)
+      add(editorForm, Position.Center)
+      add(new FlowPanel(prevButton, posLabel, nextButton), Position.South)
     }
     centerOnScreen()
   }
 
   val editorForm = new BorderPanel {
-    val top = new FlowPanel(new Label(" Left = "), leftField, new Label(" Top = "), topField, new Label(" Right = "),
-      rightField, new Label(" Bottom = "), bottomField, new Label(" Text = "), textField)
+    val top = new FlowPanel(FlowPanel.Alignment.Left)(
+      new Label(" Left, Top: "), leftTopField,
+      new Label(" Right, Bottom: "), rightBottomField,
+      new Label(" Text: "), plateTextField)
     add(top, Position.North)
-    add(imageCanvas, Position.Center)
+    add(new ScrollPane(imagePanel), Position.Center)
   }
 
-  listenTo(loadFolderButton, leftField, topField, rightField, bottomField, imageList)
+  listenTo(loadFolderButton,
+    leftTopField, rightBottomField, plateTextField,
+    prevButton, nextButton, imagePanel.mouse.clicks)
   reactions += {
     case ButtonClicked(`loadFolderButton`) => openFolderAction()
-    case EditDone(`leftField`) => println("Left: " + leftField.text)
-    case SelectionChanged(`imageList`) => println("Change image")
+    case EditDone(`leftTopField`) => updateMetadata()
+    case EditDone(`rightBottomField`) => updateMetadata()
+    case EditDone(`plateTextField`) => updateMetadata()
+    case ButtonClicked(`prevButton`) => showImage(currentImageIndex-1)
+    case ButtonClicked(`nextButton`) => showImage(currentImageIndex+1)
+    case e: MouseClicked => imageClicked(e.point)
   }
 
   /** Open folder action. */
@@ -72,8 +86,61 @@ object MainApp extends SimpleSwingApplication {
 
   def loadImageList(dir: File): Unit = {
     if (dir.isDirectory){
-      val imgs = dir.listFiles.filter(_.isFile).filter(_.getName.endsWith(".png")).map(_.getName)
-      imageList.listData = imgs
+      imageList.clear()
+      imageList ++= dir.listFiles.filter(_.isFile).filter(imageFilter)
+      metadataList.clear()
+      showImage(0)
+    }
+  }
+
+  def imageFilter(file: File): Boolean = file.getName.endsWith(".png") || file.getName.endsWith(".jpg")
+
+  /** Show image */
+  def showImage(index: Int): Unit = {
+    if(index < imageList.size && index >= 0){
+      currentImageIndex = index
+      val meta = metadataList.getOrElse(imageList(index).getName, ImageMetadata(0, 0, 0, 0, ""))
+      imagePanel.imagePath = imageList(index).getAbsolutePath
+      imagePanel.setBoxPosition(meta.left, meta.top, meta.right, meta.bottom)
+      posLabel.text = "%d/%d".format(currentImageIndex+1, imageList.size)
+      leftTopField.text = "%d, %d".format(meta.left, meta.top)
+      rightBottomField.text = "%d, %d".format(meta.right, meta.bottom)
+      plateTextField.text = meta.plateText
+      leftTopField.requestFocus()
+    }
+  }
+
+  def imageClicked(pos: Point) : Unit = {
+    if(leftTopField.hasFocus){
+      leftTopField.text = "%d, %d".format(pos.x, pos.y)
+    } else if(rightBottomField.hasFocus){
+      rightBottomField.text = "%d, %d".format(pos.x, pos.y)
+    }
+    updateMetadata()
+  }
+
+  /** Save image metadata into the file */
+  def updateMetadata() : Unit = {
+    val lt = parsePos(leftTopField.text)
+    val rb = parsePos(rightBottomField.text)
+    imagePanel.setBoxPosition(lt._1, lt._2, rb._1, rb._2)
+    if(currentImageIndex > -1 && currentImageIndex < imageList.size) {
+      val meta = ImageMetadata(lt._1, lt._2, rb._1, rb._2, plateTextField.text)
+      metadataList += (imageList(currentImageIndex).getName -> meta)
+    }
+  }
+
+  def parsePos(str: String): (Int, Int) = {
+    val tokens = str.split(",")
+    if(tokens.size != 2) (0, 0)
+    else (safeInt(tokens(0)), safeInt(tokens(1)))
+  }
+
+  def safeInt(str: String): Int = {
+    try{
+      str.trim.toInt
+    } catch {
+      case e: NumberFormatException => 0
     }
   }
 }
